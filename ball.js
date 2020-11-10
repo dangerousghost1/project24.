3f387e50a428daf013afe03699284e535515c5a9
dangerousghost1 <72561522+dangerousghost1@users.noreply.github.com>
2020-11-10 08:43:00 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,28 @@
+class Ball{
+   
+    constructor(x,y,r){
+
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+
+        }
+
+        //this.body.Bodies.circle(x,y,r,options)
+        this.body=Bodies.circle(x,y,r,options)
+        this.r=r
+        World.add(world,this.body)
+
+    }
+  display(){
+      push()
+      translate(this.body.position.x,this.body.position.y)
+      rotate(this.body.angle)
+      fill("blue");
+  circle(0,0,this.r) 
+  pop()
+
+  }
+}
---
+++ wall.js
@@ -0,0 +1,40 @@
+
+ class Wall{
+    constructor(x,y,width,height){
+    
+     var options={
+       isStatic:true
+     }
+
+   this.body=Bodies.rectangle(x,y,width,height,options)
+   this.width=width
+   this.height=height
+   World.add(world,this.body)
+
+
+    }
+ display(){
+
+   rectMode(CENTER)
+rect(this.body.position.x,this.body.position.y,this.width,this.height)
+// rect(0,0,0,0)
+ }
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
